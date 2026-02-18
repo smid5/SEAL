14aaae60c809c25ec401ea572faf18686e9f8955
smid5 <shingocresskill0515@gmail.com>
2026-02-18 19:53:46 +0000

    initial experiment on image_transformations

--- image_transformations.py
+++ image_transformations.py
@@ -13,6 +13,7 @@ import wandb
 import random
 import io
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 from optim_utils import image_distortion
 from utils import *
 import argparse
@@ -34,6 +35,13 @@ def compute_l2(image, sentence_model, k, b, seed, pipe, device):
     )
     return calculate_patch_l2(recon_noise, noise, k)
 
+def detect_watermark_from_l2(l2_list, tau, m_match):
+    """
+    Returns (detected_bool, num_matches)
+    """
+    m = sum(1 for l2 in l2_list if l2 < tau)
+    detected = (m >= m_match)
+    return detected, m
 
 if __name__ == '__main__':
 
@@ -46,9 +54,11 @@ if __name__ == '__main__':
     parser.add_argument('--end', type=int, default=1000)
     parser.add_argument('--wandb_project', type=str, default='noise-detection')
     parser.add_argument('--wandb_entity', type=str, default=None)
-    parser.add_argument('--model_id', default='stabilityai/stable-diffusion-2-1-base')
+    parser.add_argument('--model_id', default='Manojb/stable-diffusion-2-1-base')
     parser.add_argument('--online', action='store_true', default=False)
     parser.add_argument('--save_each', action='store_true', default=False)
+    parser.add_argument('--tau', type=float, default=2.3)
+    parser.add_argument('--m_match', type=int, default=12)
     args = parser.parse_args()
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -56,7 +66,7 @@ if __name__ == '__main__':
 
     # Load models
     pipe = InversableStableDiffusionPipeline.from_pretrained(
-        args.model_id, torch_dtype=torch.float16, revision='fp16').to(device)
+        args.model_id, torch_dtype=torch.float16).to(device)
     cap_processor = Blip2Processor.from_pretrained('Salesforce/blip2-flan-t5-xl')
     cap_model = Blip2ForConditionalGeneration.from_pretrained(
         'Salesforce/blip2-flan-t5-xl', torch_dtype=torch.float16).to(device)
@@ -109,10 +119,34 @@ if __name__ == '__main__':
                     else:
                         distorted_img = image_distortion(None, org_img, seed=img_idx, **dist["params"])[1] 
                     l2 = compute_l2(distorted_img, sentence_model, k, b, 42, pipe, device)
-                    all_l2[combo_key]["watermarked"][dist["name"]].append(l2)
+                    detected, num_matches = detect_watermark_from_l2(l2, tau=args.tau, m_match=args.m_match)
+                    all_l2[combo_key]["watermarked"][dist["name"]].append(int(detected))
 
                 random_image = pipe(prompt_2).images[0]
                 l2_random = compute_l2(random_image, sentence_model, k, b, 42, pipe, device)
-                all_l2[combo_key]["random"].append(l2_random)
+                detected_random, num_matches_random = detect_watermark_from_l2(l2_random, tau=args.tau, m_match=args.m_match)
+                all_l2[combo_key]["random"].append(int(detected_random))
+
+            for dist in distortions:
+                name = dist["name"]
+                acc = np.mean(all_l2[combo_key]["watermarked"][name])
+                print(name, acc)
+
+            names = [d["name"] for d in distortions]
+            accs = [np.mean(all_l2[combo_key]["watermarked"][n]) for n in names]
+
+            plt.figure(figsize=(10,5))
+            plt.bar(names, accs)
+            plt.ylim(0, 1.05)
+            plt.ylabel("Detection Accuracy")
+            plt.xlabel("Image Transformation")
+            plt.title(f"Robustness (k={k}, b={b}, tau={args.tau}, m_match={args.m_match})")
+
+            for i, v in enumerate(accs):
+                plt.text(i, v + 0.02, f"{v:.3f}", ha="center")
+
+            plt.tight_layout()
+            plt.savefig(os.path.join(args.output_dir, f"robustness_{combo_key}.png"))
+            plt.show()
 
     wandb.finish()
